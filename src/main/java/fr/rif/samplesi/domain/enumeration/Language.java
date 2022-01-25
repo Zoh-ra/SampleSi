87a6082729b12290d2d096b8f94a9e1b169d8840
Zohra <raitonoums@gmail.com>
2022-01-25 20:14:39 +0100

    ajout d'un paragraphe dans index.html et des jours de la semaine dans planning.txt

---
+++ src/main/java/fr/rif/samplesi/domain/enumeration/Language.java
@@ -0,0 +1,10 @@
+package fr.rif.samplesi.domain.enumeration;
+
+/**
+ * The Language enumeration.
+ */
+public enum Language {
+    FRENCH,
+    ENGLISH,
+    SPANISH,
+}
---
+++ src/main/java/fr/rif/samplesi/repository/CountryRepository.java
@@ -0,0 +1,12 @@
+package fr.rif.samplesi.repository;
+
+import fr.rif.samplesi.domain.Country;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data SQL repository for the Country entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {}
